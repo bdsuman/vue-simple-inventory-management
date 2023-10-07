953003b0b0fb2399baa00c68ba2196d03a8670cb
Suman Sen <mesuman@yahoo.com>
2023-10-08 02:02:03 +0600

    update last

--- src/layouts/Header.vue
+++ src/layouts/Header.vue
@@ -3,6 +3,9 @@ import { onMounted } from 'vue';
 import { RouterLink } from 'vue-router';
 import { initFlowbite } from 'flowbite';
 
+import authStore from '../stores/authStore';
+const auth = authStore();
+
 onMounted(() => {
     initFlowbite();
 })
@@ -22,7 +25,7 @@ onMounted(() => {
          </button>
         <router-link to="/" class="flex ml-2 md:mr-24">
           <img src="https://flowbite.com/docs/images/logo.svg" class="h-8 mr-3" alt="FlowBite Logo" />
-          <span class="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap dark:text-white">Inventory</span>
+          <span class="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap dark:text-white">Invextory</span>
         </router-link>
       </div>
       <div class="flex items-center">
@@ -30,21 +33,21 @@ onMounted(() => {
             <div>
               <button type="button" class="flex text-sm bg-gray-800 rounded-full focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" aria-expanded="false" data-dropdown-toggle="dropdown-user">
                 <span class="sr-only">Open user menu</span>
-                <img class="w-8 h-8 rounded-full" src="https://flowbite.com/docs/images/people/profile-picture-2.jpg" alt="user photo">
+                <img class="w-8 h-8 rounded-full" :src="auth.authenticatedUser.image" alt="user photo">
               </button>
             </div>
             <div class="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded shadow dark:bg-gray-700 dark:divide-gray-600" id="dropdown-user">
               <div class="px-4 py-3" role="none">
                 <p class="text-sm text-gray-900 dark:text-white" role="none">
-                  Suman Sen
+                  {{ `${auth.authenticatedUser.firstName} ${auth.authenticatedUser.lastName}` }}
                 </p>
                 <p class="text-sm font-medium text-gray-900 truncate dark:text-gray-300" role="none">
-                    mesumanbd@gmail.com
+                  {{ auth.authenticatedUser.email }}
                 </p>
               </div>
               <ul class="py-1" role="none">
                 <li>
-                  <router-link to="/logout" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white" role="menuitem">Logout</router-link>
+                  <button @click="auth.logout()"  class="w-full block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white" role="menuitem">Logout</button>
                 </li>
               </ul>
             </div>
