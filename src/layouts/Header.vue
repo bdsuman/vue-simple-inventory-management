60049acfeba51c79d591a1154739e98571ba55d5
Suman Sen <mesuman@yahoo.com>
2023-10-07 23:14:42 +0600

    update

--- src/layouts/Header.vue
+++ src/layouts/Header.vue
@@ -22,7 +22,7 @@ onMounted(() => {
          </button>
         <router-link to="/" class="flex ml-2 md:mr-24">
           <img src="https://flowbite.com/docs/images/logo.svg" class="h-8 mr-3" alt="FlowBite Logo" />
-          <span class="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap dark:text-white">Invextory</span>
+          <span class="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap dark:text-white">Inventory</span>
         </router-link>
       </div>
       <div class="flex items-center">
